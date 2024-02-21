eba71089fb1ccce8956500c19bc9a141c3f5c194
Christopher Leader <leaderc@oregonstate.edu>
2024-02-21 17:35:33 -0500

    Day 1 complete.

---
+++ day1/day1.playground/Contents.swift
@@ -0,0 +1,85 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+//var name = "Ted"
+//name = "Rebecca"
+//name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+//How to store whole numbers
+let score = 10
+let reallyBig = 100000000
+//let reallyBig = 100_000_000
+//let reallyBig = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let sqauredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+counter -= 10
+counter /= 2
+
+//let number = 120
+//print(number.isMultiple(of:3))
+print(120.isMultiple(of: 3))
+
+//How to store decimal numbers
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name = "Nicolas Cage"
+name = "John Travolta"
+
+//var name = "Nicolas Cage"
+//name = 57
+
+var rating = 5.0
+rating *= 2
